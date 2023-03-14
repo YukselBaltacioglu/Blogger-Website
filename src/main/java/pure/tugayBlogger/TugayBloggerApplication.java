0c8a485146d01f2c45f959e4d8086d3aa8359f68
YukselBaltacioglu <ykselbaltacioglu@gmail.com>
2023-03-14 15:43:16 +0300

    Blogger

---
+++ src/main/java/pure/tugayBlogger/TugayBloggerApplication.java
@@ -0,0 +1,13 @@
+package pure.tugayBlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugayBloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugayBloggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pure/tugayBlogger/TugayBloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package pure.tugayBlogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugayBloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
